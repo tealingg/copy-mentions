7495f19684f954d8bab9dccda402164df0954fc6
tealingg <tealingg@gmail.com>
2021-02-15 16:53:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const { clipboard } = require("electron");
 module.exports = class CopyMention extends Plugin {
     async startPlugin() {
         const GuildChannelUserContextMenu = await getModule(m => m.default && m.default.displayName == "GuildChannelUserContextMenu");
+        const guildUserName = GuildChannelUserContextMenu.default.displayName
         const ChannelListTextChannelContextMenu = await getAllModules((m) => m.default && m.default.displayName == "ChannelListTextChannelContextMenu", false)[2];
+        const channelName = ChannelListTextChannelContextMenu.default.displayName
         const DMUserContextMenu = await getModule(m => m.default && m.default.displayName == "DMUserContextMenu");
+        const dmUserName = DMUserContextMenu.default.displayName
         const DeveloperContextMenu = await getModule(m => m.default && m.default.displayName == "DeveloperContextMenu");
+        const roleName = DeveloperContextMenu.default.displayName
         const { MenuItem } = await getModule([ "MenuItem" ]);
         console.log(GuildChannelUserContextMenu);
         inject("tealingg-copy-mention-user-guild", GuildChannelUserContextMenu, "default", (args, res) => {
@@ -61,6 +65,10 @@ module.exports = class CopyMention extends Plugin {
             ];
             return res;
         });
+        GuildChannelUserContextMenu.default.displayName = guildUserName;
+        DMUserContextMenu.default.displayName = dmUserName;
+        ChannelListTextChannelContextMenu.default.displayName = channelName;
+        DeveloperContextMenu.default.displayName = roleName;
     }
     pluginWillUnload() {
         uninject("tealingg-copy-mention-user-guild");
